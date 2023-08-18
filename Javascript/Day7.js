82a45e21a76c87f11ab8e44c862e7ae73396a2bb
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-18 15:12:46 +0530

    Day7 Commit

---
+++ Javascript/Day7.js
@@ -0,0 +1,38 @@
+let x = 0o124;
+let y = 0xA3;
+let z = 0b101;
+console.log(x,y,z);
+
+x= 1e6;
+console.log(x);
+
+x=1;
+if(!Number.isNaN(x)) console.log("not number");
+
+console.log(Math.random());
+console.log(Math.round(Math.random()*10,1));
+console.log(Math.round(Math.random()*100,1));
+num1 = 2323000012;
+console.log(num1.toExponential());
+console.log(Number(1.3334).toFixed(2));
+
+let obj = new Date("2023 07 25 12:13:56");
+console.log(obj.getMonth());
+console.log(obj.getFullYear());
+console.log(obj.getTime());
+console.log(obj.getHours());
+
+obj.setMonth(5);
+console.log(obj.getMonth());
+
+function getDigitalTime(){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    return hours + ':' + minutes + ':' +seconds;
+
+}
+
+console.log(getDigitalTime());
