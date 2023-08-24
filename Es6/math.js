1e2688ee9e5137e6d799c85f28d32df5734c3202
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-24 18:17:32 +0530

    math functions in js

---
+++ Es6/math.js
@@ -0,0 +1,35 @@
+console.log(Math.trunc(5));
+console.log(Math.trunc(5.7));
+console.log(Math.trunc(5.1));
+console.log(Math.trunc(-5.9));
+
+
+console.log(Math.sign(5));
+console.log(Math.sign(-5));
+console.log(Math.sign(0));
+
+
+console.log(Math.cbrt(64));
+console.log(Math.cbrt(125));
+
+
+console.log(Math.log2(1));
+console.log(Math.log10(10));
+
+
+let x=Number.EPSILON;
+console.log(x);
+
+
+let y=Number.MAX_SAFE_INTEGER;
+console.log(y);
+
+
+let z=Number.MIN_SAFE_INTEGER;
+console.log(z);
+
+
+let num1=12;
+let num2=4890385894730964354345242;
+console.log(Number.isInteger(num1));
+console.log(Number.isSafeInteger(num2));
