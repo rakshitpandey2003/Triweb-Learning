fc410a3b313e4972b85f23ba8e9523f300da3f33
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-24 18:13:52 +0530

    Classes in js

---
+++ Es6/class.js
@@ -0,0 +1,11 @@
+class student{
+    constructor(sname,classId){
+        this.sname=sname;
+        this.classId=classId;
+    }
+}
+
+const student1= new student("Rakshit",12);
+const student2= new student("Raman",9);
+console.log(student1);
+console.log(student2);
