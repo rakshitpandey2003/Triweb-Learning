e9fcfc37d18972d532eb0e2a8c0d3435a77e806c
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-26 17:39:47 +0530

    calulating using rest api

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,3 +1,15 @@
-import express from "express";
+const express = require('express');
 const app = express();
+app.use(express.json());
+
+app.get('/' ,(req , res)=>{
+    res.send("this is try");
+});
+
+app.get('/add' ,(req , res)=>{
+    res.send("this is add");
+});
+app.post('/add' , (req , res)=>{
+    res.send(`sum is ${req.body.num1 + req.body.num2}`);
+});
 app.listen(3000);
