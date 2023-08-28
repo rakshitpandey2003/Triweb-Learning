50d03dd149d943fa44dcc49a190ce7b62bda2bd3
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-28 08:16:59 +0530

    SQL data fetch using router->Main app

---
+++ NodeJS/sqlapp.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userRoute = require("./router/user");
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("this is try");
+});
+app.use('/user',userRoute);
+app.listen(3000);
