45e35af7aa16d8bbb6e1298c320fc187d9760f7d
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-28 19:13:10 +0530

    controller and model files

--- NodeJS/controllers/user.js
+++ NodeJS/controllers/user.js
@@ -1,3 +1,44 @@
-module.exports.register = (req, res) => {
-    res.send(req.body);
-};
+const userModel=require('../models/user');
+
+//register
+module.exports.register=async(req,res)=>{
+    try {
+        let result=await userModel.create(req.body);
+        res.send({status:"success",data:{id:result.dataValues.id}});
+    } catch (error) {
+        res.send({status:"error",message:"User registration failed"});
+    }
+}
+
+//get
+module.exports.get=async(req,res)=>{
+    try {
+        let data=await userModel.findByPk(req.body.id);
+        res.send({status:"success",data:data});
+    } catch (error) {
+        res.send({status:"error",message:"User not found"});
+    }
+}
+
+//update
+module.exports.update=async(req,res)=>{
+    try{
+        let user=await userModel.findByPk(req.body.id);
+        user.password=req.body.password;
+        await user.save();
+        res.send({status:"success",message:"User data updated"});
+    }catch{
+        res.send({status:"error",message:"User data not updated"});
+    }
+}
+
+//delete
+module.exports.delete=async(req,res)=>{
+    try {
+        let user=await userModel.findByPk(req.body.id);
+        await user.destroy();
+        res.send({status:"success",data:{message:"User deleted"}});
+    } catch (error) {
+        res.send({status:"error",message:"User not deleted"}); 
+    }
+}
