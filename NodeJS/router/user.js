c434c31fbcdcc6841d4a9f90816474d359e17154
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-28 19:12:52 +0530

    router file for management

--- NodeJS/router/user.js
+++ NodeJS/router/user.js
@@ -3,5 +3,8 @@ const userController = require('../controllers/user');
 const router = express.Router();
 
 router.post('/register' , userController.register);
+router.post('/get' , userController.get);
+router.post('/update' , userController.update);
+router.post('/delete' , userController.delete);
 
 module.exports = router;
