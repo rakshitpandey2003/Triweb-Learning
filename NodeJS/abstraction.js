b8760f51f58a8155784d5e309dbaed7fd1aaa09f
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-08-29 19:33:31 +0530

    practised abstraction

---
+++ NodeJS/abstraction.js
@@ -0,0 +1,12 @@
+class musicPlayer{
+    start=()=>{
+        console.log("Playing...");
+    }
+    pause=()=>{
+        console.log("Paused...");
+    }
+}
+
+let obj=new musicPlayer();
+obj.start();
+obj.pause();
