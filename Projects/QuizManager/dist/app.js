41e6cd08207a86ed50f0f825229256c880b3e277
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-09-04 17:31:24 +0530

    conn. with database established

--- Projects/QuizManager/dist/app.js
+++ Projects/QuizManager/dist/app.js
@@ -3,9 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 const express_1 = __importDefault(require("express"));
+const user_1 = __importDefault(require("./routes/user"));
 const app = (0, express_1.default)();
+const connectionstring = "mongodb+srv://Myuser:1234@mycluster.ywsim2g.mongodb.net/quizdata?retryWrites=true&w=majority";
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("hello");
 });
+app.use("/user", user_1.default);
+mongoose_1.default.connect(connectionstring);
 app.listen(3000);
--- Projects/QuizManager/src/app.ts
+++ Projects/QuizManager/src/app.ts
@@ -1,6 +1,10 @@
+import mongoose, { mongo } from 'mongoose';
 import express from "express";
 import UserRoute from "./routes/user";
+
 const app = express();
+
+const connectionstring = "mongodb+srv://Myuser:1234@mycluster.ywsim2g.mongodb.net/quizdata?retryWrites=true&w=majority";
 
 app.use(express.json());
 
@@ -9,4 +13,7 @@ app.get("/",(req , res)=>{
 });
 
 app.use("/user" , UserRoute);
+
+mongoose.connect(connectionstring);
+
 app.listen(3000);
