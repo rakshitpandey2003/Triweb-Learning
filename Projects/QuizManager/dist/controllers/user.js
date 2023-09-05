844bfd586fee3389fdac8ff77472e6517d4a7fc1
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-09-05 19:35:13 +0530

    added schema and interface

--- Projects/QuizManager/dist/controllers/user.js
+++ Projects/QuizManager/dist/controllers/user.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerUser = void 0;
+const user_1 = __importDefault(require("../models/user"));
 const registerUser = (req, res) => {
-    console.log(req.body);
+    const user = new user_1.default(req.body);
+    user.save();
     res.send("Done");
 };
 exports.registerUser = registerUser;
--- Projects/QuizManager/src/controllers/user.ts
+++ Projects/QuizManager/src/controllers/user.ts
@@ -1,8 +1,40 @@
-import { Request , Response } from "express";
+import { Request, Response } from "express";
+import User from '../models/user';
 
-const registerUser = (req:Request , res:Response)=>{
-    console.log(req.body);
+interface ReturnResponse{
+    status:"success" | "error",
+    message:String,
+    data:{}
+}
+
+const registerUser = async (req: Request, res: Response) => {
+    let resp:ReturnResponse;
+    try {
+        const user = new User(req.body);
+        const result = await user.save();
+        if (!result) {
+            resp = {
+                status:"error" , 
+                message:"No result found",
+                data:{}
+            }
+        } else {
+            resp = {
+                status:"success" , 
+                message:"Registration Done",
+                data:{userId:result._id}
+            }
+        }
+        res.send(resp);
+    } catch (error) {
+        resp = {
+            status:"error" , 
+            message:"something went wrong",
+            data:{}
+        }
+        res.status(500).send(resp);
+    }
     res.send("Done");
 }
 
-export {registerUser};
+export { registerUser };
