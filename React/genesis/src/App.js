dd2c7c68312dfccaa4486321522f51c00131a794
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-09-12 18:12:43 +0530

    first component

--- React/genesis/src/App.js
+++ React/genesis/src/App.js
@@ -1,16 +1,10 @@
+import First from "./components/First";
+
 function App() {
   return (
-    <div className="App">
-     <div>
-      <h1>MY app</h1>
-      <p>my application</p>
-      <button>my button</button>
-     </div>
-     <div>
-      <h1>MY app</h1>
-      <p>my application</p>
-      <button>my button</button>
-     </div>
+    <div>
+      <First/>
+    <First/>
     </div>
   );
 }
---
+++ React/genesis/src/components/First.js
@@ -0,0 +1,8 @@
+function First(){
+return <div>
+      <h1>MY app</h1>
+      <p>my application</p>
+      <button>my button</button>
+     </div>
+}
+export default First;
