a000ee7565a9107580ce17ed078e462df142147b
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-09-12 18:07:54 +0530

    basic react app setup

---
+++ React/genesis/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <h1>MY app</h1>
+      <p>my application</p>
+      <button>my button</button>
+     </div>
+     <div>
+      <h1>MY app</h1>
+      <p>my application</p>
+      <button>my button</button>
+     </div>
+    </div>
+  );
+}
+
+export default App;
