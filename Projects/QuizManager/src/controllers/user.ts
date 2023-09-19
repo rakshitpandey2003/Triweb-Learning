6f14fb4e787f19bf63800a9d83e5bb4381bcaf3e
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-09-19 15:33:53 +0530

    we can get user now by token only

--- Projects/QuizManager/src/controllers/user.ts
+++ Projects/QuizManager/src/controllers/user.ts
@@ -2,18 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import User from '../models/user';
 import ProjectError from "../helper/error";
 import { ReturnResponse } from "../utils/interfaces";
+import jwt from "jsonwebtoken";
 
 const getUser=async (req:Request,res:Response , next:NextFunction)=>{
     let resp:ReturnResponse;
-    console.log(req.userId);
     try {
-        if(req.userId != req.params.userId){
-            const err=new ProjectError("You are not authorized");
-            err.statusCode=401;
-            err.data={hi:"it's error"};
-            throw err;
-        }
-        const userId=req.params.userId;
+        // if(req.userId != req.params.userId){
+        //     const err=new ProjectError("You are not authorized");
+        //     err.statusCode=401;
+        //     err.data={hi:"it's error"};
+        //     throw err;
+        if(req.headers.authorization){
+        const token=req.headers.authorization.split(' ')[1];
+        const decoded=<any>jwt.verify(token, 'myverysecretkey');
+        const userId=decoded.userId
         const user= await User.findById(userId, {name:1, email:1 });
         if(!user){
             const err=new ProjectError("User Not Found");
@@ -24,6 +26,8 @@ const getUser=async (req:Request,res:Response , next:NextFunction)=>{
             resp={status:"success", message:"User Found", data:{user:user}};
             res.status(200).send(resp);
         }
+        }
+        
     } catch (error) {
         next(error);
     }
