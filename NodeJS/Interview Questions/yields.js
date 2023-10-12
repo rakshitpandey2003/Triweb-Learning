704c993e92af0020934123ff2a1977999c43d020
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-10-12 18:50:37 +0530

    learned about yield and generator function

---
+++ NodeJS/Interview Questions/yields.js
@@ -0,0 +1,13 @@
+function* getNum(index){
+    while(index < 5){
+        yield index;
+        index++;
+    }
+}
+let it  = getNum(0);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().done);
