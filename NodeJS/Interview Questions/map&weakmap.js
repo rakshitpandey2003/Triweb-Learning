8ef2c1b7cb2af4d2cd2ff3407f902d7c1a289b7c
rakshitpandey2003 <rakshitpandey012@gmail.com>
2023-10-16 18:29:26 +0530

    implemented map and weak map

---
+++ NodeJS/Interview Questions/map&weakmap.js
@@ -0,0 +1,11 @@
+let user = {
+    uname: "Ms abc",
+    mobile: 999999
+    }
+    let key1 = {};
+    let key2 = {uname: "Rakshit"};
+    let weakMapExample = {
+    [key1]: "Some data",
+    [key2]: "New Data"
+    }
+    console.log(weakMapExample);
